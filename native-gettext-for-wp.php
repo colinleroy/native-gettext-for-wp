72cb4d9c7616e191f6ee4fcdfd2d7fc19d950a6e
Colin Leroy-Mira <colin@colino.net>
2022-02-23 22:24:55 +0100

    Fix plugin name for Wordpress rules

--- native-gettext-for-wp.php
+++ native-gettext-for-wp.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:  Native Gettext for Wordpress
+ * Plugin Name:  Native Gettext
  * Plugin URI:   https://github.com/colinleroy/native-gettext-for-wp
  * Description:  A minimal native gettext implementation.
  * Version:      1.0.0
@@ -9,7 +9,7 @@
  * License:      GPL-3.0+
  * License URI:  http://www.gnu.org/licenses/gpl-3.0.html
  *
- * @package   Native Gettext for Wordpress
+ * @package   Native Gettext
  * @copyright Copyright (c) 2022, Colin Leroy-Mira
  * @license   GPL-3.0+
  * @since     0.1.0
